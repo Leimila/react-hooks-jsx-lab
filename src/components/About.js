8a54a836ba156442f16d4299bd06beb880d49a34
Leimila <leimilamitei2016@gmail.com>
2024-11-28 00:20:52 +0300

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Import variable from user.js
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm a web developer passionate about creating interactive applications and experiences on the web.</p>
+      <img src={image} alt="I made this" /> {/* Use imported variable */}
+    </div>
+  );
 }
 
 export default About;
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <a href="#home">Home</a>   {/* Anchor link to Home */}
+      <a href="#about">About</a> {/* Anchor link to About */}
+    </nav>
+  );
 }
 
 export default NavBar;
+
